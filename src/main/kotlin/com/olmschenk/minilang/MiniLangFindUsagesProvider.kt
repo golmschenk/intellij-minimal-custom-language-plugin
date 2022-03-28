da06c72e65b7a594e6136749719fae7615f2e540
Greg Olmschenk <greg@olmschenk.com>
2022-03-28 19:33:26 -0400

    Add a find usages provider for the currently not working references

---
+++ src/main/kotlin/com/olmschenk/minilang/MiniLangFindUsagesProvider.kt
@@ -0,0 +1,59 @@
+package com.olmschenk.minilang
+
+import com.intellij.lang.cacheBuilder.DefaultWordsScanner
+import com.intellij.lang.cacheBuilder.WordsScanner
+import com.intellij.lang.findUsages.FindUsagesProvider
+import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiNamedElement
+import com.intellij.psi.tree.TokenSet
+import com.olmschenk.minilang.parser.MiniLangLexer
+import com.olmschenk.minilang.psi.MiniLangVariableIdentifier
+import org.antlr.intellij.adaptor.lexer.ANTLRLexerAdaptor
+import org.antlr.intellij.adaptor.lexer.PSIElementTypeFactory
+
+
+class MiniLangFindUsagesProvider : FindUsagesProvider {
+    override fun getWordsScanner(): WordsScanner? {
+        return DefaultWordsScanner(
+            ANTLRLexerAdaptor(MiniLangLanguage.INSTANCE, MiniLangLexer(null)),
+            PSIElementTypeFactory.createTokenSet(
+                MiniLangLanguage.INSTANCE,
+                MiniLangLexer.VARIABLE_IDENTIFIER
+            ),
+            TokenSet.EMPTY,
+            TokenSet.EMPTY
+        )
+    }
+
+    override fun canFindUsagesFor(psiElement: PsiElement): Boolean {
+        return psiElement is PsiNamedElement
+    }
+
+    override fun getHelpId(psiElement: PsiElement): String? {
+        return null
+    }
+
+    override fun getType(element: PsiElement): String {
+        return if (element is MiniLangVariableIdentifier) {
+            "variable identifier"
+        } else {
+            ""
+        }
+    }
+
+    override fun getDescriptiveName(element: PsiElement): String {
+        return if (element is MiniLangVariableIdentifier) {
+            element.text
+        } else {
+            ""
+        }
+    }
+
+    override fun getNodeText(element: PsiElement, useFullName: Boolean): String {
+        return if (element is MiniLangVariableIdentifier) {
+            element.text
+        } else {
+            ""
+        }
+    }
+}
